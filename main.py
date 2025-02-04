41bbdf814d6b4cefb3223dce341d716da0f16df0
JumaOchi <ochiengjuma95@gmail.com>
2025-02-04 05:05:25 +0300

    astroid functionality and player shoot done

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 
 def main() :
@@ -8,18 +11,44 @@ def main() :
 
     print("Starting asteroids!")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    #Creating two groups for the game objects
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    #Setting up player container
+    Player.containers = (updatable, drawable)
+    #Setting up asteroid container
+    Asteroid.containers = (updatable, drawable, asteroids)
+    #Setting up asteroid field container
+    AsteroidField.containers = (updatable)
     time = pygame.time.Clock()
     dt = 0
+    #Creating the player object
     player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+    #Creating the asteroid field object
+    asteroid_field = AsteroidField()
+    # Set up a new group in your initialization code that contains all of your shots.
+    shots_group = pygame.sprite.Group()
+    Shot.containers = (updatable, drawable, shots_group)
 
+    #Main game loop
     while True :
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+
         #Hook the update method into the game loop by calling it on the player object each frame before rendering
-        player1.update(dt)
-        player1.draw(screen) #drawing player on screen/ rendering
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)#drawing player on screen/ rendering
+        #iterate over all of the objects in your asteroids group. 
+        # Check if any of them collide with the player. If a collision
+        #  is detected, the program should print Game over on screen! and immediately exit the program
+        for asteroid in asteroids:
+            if player1.collision(asteroid):
+                print("Game over!")
+                return
         pygame.display.flip()
         time.tick(60)
         dt = time.tick(60) / 1000.0
--- player.py
+++ player.py
@@ -1,12 +1,13 @@
 import pygame
 from circleshape import CircleShape
 from constants import *
+from shot import Shot
 
 class Player(CircleShape) :
     def __init__(self, x, y, radius) :
         super().__init__(x, y, radius)
         self.rotation = 0
-    
+        self.shoot_cooldown = 0 #new variable to act as timer
 
     # in the player class
     def triangle(self):
@@ -37,9 +38,21 @@ class Player(CircleShape) :
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            #prevents the player from shooting again until the cooldown is
+            if self.shoot_cooldown <= 0:
+                self.shoot()
+                self.shoot_cooldown = PLAYER_SHOOT_COOLDOWN
+        #decrements the cooldown timer
+        self.shoot_cooldown -= dt
 
     def move(self, dt) :
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
-        
+    
+    def shoot(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        position = self.position + forward * self.radius
+        shot = Shot(position.x, position.y, SHOT_RADIUS)
+        shot.velocity = forward * PLAYER_SHOOT_SPEED
 
